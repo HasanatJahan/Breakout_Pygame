79b9c64d8b040991c7c0cc2424ec36dfb64929f8
Hasanat Jahan <jahanhasanat@gmail.com>
2019-01-21 01:36:45 -0500

    getting the bricks fixed into objects so they can interact withthe ball

--- Bricks.py
+++ Bricks.py
@@ -12,17 +12,11 @@ class Bricks:
 		self.brick_list = []
 		self.bricks_y = 35
 		self.bricks_x = 35
-		# self.brick_unit = pygame.Rect(self.bricks_x, self.bricks_y, self.brick_width, self.brick_height)
 
 	#--This adds on rectangle objects to a list of bricks
 	def draw(self):
-		self.brick_list = []
-		# global brick_list
-		# global bricks_x
-		# global bricks_y
-		# global brick_unit
-		# self.bricks_y = 35
-		
+		self.brick_list.clear()
+		self.bricks_y = 35
 		for i in range(7):
 			self.bricks_x = 35
 			for j in range(8):
@@ -30,7 +24,6 @@ class Bricks:
 				self.brick_list.append(pygame.Rect(self.bricks_x, self.bricks_y, self.brick_width, self.brick_height))
 				self.bricks_x += self.brick_width + 10
 			self.bricks_y += self.brick_height + 5
-			print(self.brick_list)
 			# break
 
 	#--This draws on the bricks from the list, with each object being brick
